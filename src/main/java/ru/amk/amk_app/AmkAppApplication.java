b4c0eea38ea8dfe1d782a70dfdc8847db36a6138
Kasmudin <kasmudin@mail.ru>
2024-07-26 22:34:08 +0300

    echo "# AMK_App" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/kasmudin/AMK_App.git git push -u origin main

---
+++ src/main/java/ru/amk/amk_app/AmkAppApplication.java
@@ -0,0 +1,13 @@
+package ru.amk.amk_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmkAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmkAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/amk/amk_app/AmkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.amk.amk_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmkAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
